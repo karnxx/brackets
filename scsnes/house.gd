99c273f04e882934564e63eb9cb9666191449c14
karna amborkar <fastestkyo@gmail.com>
2026-08-23 18:49:30 +0530

    added blackout mechanics enhancement

--- scsnes/house.gd
+++ scsnes/house.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
+# 1 =lights on
+# 0 =lights off
+var state = 1
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	light_crc()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func light_crc():
+	while true:
+		if state == 0:
+			await get_tree().create_timer(5.0).timeout
+			state = 1
+			print("LIGHTS ON")
+		elif state == 1:
+			await get_tree().create_timer(10.0).timeout
+			state = 0
+			print("LIGHTS OFF")
