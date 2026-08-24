da7c14ff795352b82c9f6adbcd247db19f4483f9
karna amborkar <fastestkyo@gmail.com>
2026-08-24 21:49:54 +0530

    headache

--- scsnes/liv.gd
+++ scsnes/liv.gd
@@ -7,8 +7,8 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group('plr') and is_instance_valid(body):
 		body.room = room
-		#get_parent().get_node('player/Camera2D').limit_left = cords[0]
-		#get_parent().get_node('player/Camera2D').limit_right = cords[1]
-		#get_parent().get_node('player/Camera2D').limit_top = cords[2]
-		#get_parent().get_node('player/Camera2D').limit_bottom = cords[3]
-		#body.set_camera_to_room()
+		get_parent().get_node('player/Camera2D').limit_left = cords[0]
+		get_parent().get_node('player/Camera2D').limit_right = cords[1]
+		get_parent().get_node('player/Camera2D').limit_top = cords[2]
+		get_parent().get_node('player/Camera2D').limit_bottom = cords[3]
+		body.set_camera_to_room()
