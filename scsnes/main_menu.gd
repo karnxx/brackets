ce9755270b29df122081811c142ea3df153e6f7f
Krishiv <vyaskrishiv919@gmail.com>
2026-08-26 22:08:17 +0530

    Add settings screen and menu navigation

--- scsnes/main_menu.gd
+++ scsnes/main_menu.gd
@@ -46,3 +46,6 @@ func _on_play_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+	
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://scsnes/settings.tscn")
